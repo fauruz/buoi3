994a3f318fa85663681bdd528da67d5a71d7564a
fauruz <le.phong1456@gmail.com>
2023-08-30 13:45:14 +0700

    first commit

---
+++ SinhVien.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    internal class SinhVien
+    {
+        private int masv;
+        private string hoten;
+        private double diemlt, diemth;
+    }
+}
